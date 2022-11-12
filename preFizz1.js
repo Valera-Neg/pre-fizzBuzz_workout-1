de0567eb42ebd98d21b66eeb59715bdae2a5592c
Valera-Neg <nehovora.val@gmail.com>
2022-11-12 17:56:00 -0500

    first commit

---
+++ preFizz1.js
@@ -0,0 +1,7 @@
+let preFizz = n => [...Array(n)].map((e, i) => i+1);
+
+console.log(preFizz(1)) //> [1]
+console.log(preFizz(2)) //> [1,2]
+console.log(preFizz(3)) //>  [1,2,3]
+console.log(preFizz(4)) //> [1,2,3,4]
+console.log(preFizz(5)) //> [1,2,3,4,5]
